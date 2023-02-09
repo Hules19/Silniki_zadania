3ab8d4facb7f68c79dd9a4f7fdc36ead91ce5c60
Aiden <unnamedrealhero@gmail.com>
2023-02-09 17:19:58 +0100

    4

---
+++ Silniki-zadania/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private TMP
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void ChangeScenes()
+    {
+    
+    }
+    
+    void Quit()
+    {
+    Application.Quit();
+    }
+    
+}
